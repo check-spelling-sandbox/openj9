8c0bdbb2bba9db0ed6393e662dfbbc46e07af63b
Josh Soref <jsoref@gmail.com>
2021-06-06 06:18:00 -0400

    spelling: breakpoint

--- runtime/compiler/runtime/RelocationRecord.cpp
+++ runtime/compiler/runtime/RelocationRecord.cpp
@@ -419,7 +419,7 @@ struct TR_RelocationRecordBlockFrequencyBinaryTemplate: public TR_RelocationReco
    uintptr_t _frequencyOffset;
    };
 
-struct TR_RelocationRecordBreapointGuardBinaryTemplate : public TR_RelocationRecordWithInlinedSiteIndexBinaryTemplate
+struct TR_RelocationRecordBreakpointGuardBinaryTemplate : public TR_RelocationRecordWithInlinedSiteIndexBinaryTemplate
    {
    UDATA _destinationAddress;
    };
@@ -6275,13 +6275,13 @@ TR_RelocationRecordBreakpointGuard::preparePrivateData(TR_RelocationRuntime *rel
 void
 TR_RelocationRecordBreakpointGuard::setDestinationAddress(TR_RelocationTarget *reloTarget, uintptr_t destinationAddress)
    {
-   reloTarget->storeRelocationRecordValue(destinationAddress, (uintptr_t *) &((TR_RelocationRecordBreapointGuardBinaryTemplate *)_record)->_destinationAddress);
+   reloTarget->storeRelocationRecordValue(destinationAddress, (uintptr_t *) &((TR_RelocationRecordBreakpointGuardBinaryTemplate *)_record)->_destinationAddress);
    }
 
 uintptr_t
 TR_RelocationRecordBreakpointGuard::destinationAddress(TR_RelocationTarget *reloTarget)
    {
-   return reloTarget->loadRelocationRecordValue((uintptr_t *) &((TR_RelocationRecordBreapointGuardBinaryTemplate *)_record)->_destinationAddress);
+   return reloTarget->loadRelocationRecordValue((uintptr_t *) &((TR_RelocationRecordBreakpointGuardBinaryTemplate *)_record)->_destinationAddress);
    }
 
 TR_RelocationErrorCode
@@ -6472,7 +6472,7 @@ uint32_t TR_RelocationRecord::_relocationRecordHeaderSizeTable[TR_NumExternalRel
    sizeof(TR_RelocationRecordInlinedMethodBinaryTemplate),                           // TR_InlinedStaticMethod                          = 104
    sizeof(TR_RelocationRecordInlinedMethodBinaryTemplate),                           // TR_InlinedSpecialMethod                         = 105
    sizeof(TR_RelocationRecordInlinedMethodBinaryTemplate),                           // TR_InlinedAbstractMethod                        = 106
-   sizeof(TR_RelocationRecordBreapointGuardBinaryTemplate),                          // TR_Breakpoint                                   = 107
+   sizeof(TR_RelocationRecordBreakpointGuardBinaryTemplate),                          // TR_Breakpoint                                   = 107
    sizeof(TR_RelocationRecordWithInlinedSiteIndexBinaryTemplate),                    // TR_InlinedMethodPointer                         = 108
    0,                                                                                // TR_VMINLMethod                                  = 109
    sizeof(TR_RelocationRecordValidateJ2IThunkFromMethodBinaryTemplate),              // TR_ValidateJ2IThunkFromMethod                   = 110
